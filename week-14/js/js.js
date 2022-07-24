e8050bb8b69104192297eb45e0e9552f9119ebe4
Alper Korkmaz <dr.alperkorkmaz@gmail.com>
2022-07-24 18:19:34 +0200

    vscode push update

--- week-14/js/js.js
+++ week-14/js/js.js
@@ -1,26 +1,37 @@
+
+var actualIndex  ;
+var newIndex;
+if(actualIndex==null){
+    actualIndex=0;
+    newIndex=0;
+}
 const indicators = document.getElementsByClassName("indicator");
-indicators[0].classList.add("active");
+indicators[actualIndex].classList.add("active");
 
 const slides = document.getElementsByClassName("slide");
-slides[0].classList.add("active")
+slides[actualIndex].classList.add("active")
 
-var actualIndex = 0 ;
+
 
 function changeImage(step){
-
-    var newIndex = (actualIndex+step) % (indicators.length) ;
+    newIndex = actualIndex+step;
+    if(newIndex<0){
+        newIndex=indicators.length-1;
+    }
+    if(newIndex>indicators.length-1){
+        newIndex=0;
+    }
     console.log(newIndex);
-
-    springToImage(newIndex);
-  
+    console.log(actualIndex);
+    springToImage(newIndex); 
 }
 
 function springToImage(newIndex) {
     indicators[actualIndex].classList.remove("active");
     slides[actualIndex].classList.remove("active");
-
     indicators[newIndex].classList.add("active");
     slides[newIndex].classList.add("active");
-    
     actualIndex = newIndex;
+
 }
+
